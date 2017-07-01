fbe98165201522c78e840b175fa8316fece0ba91
Matt Stanley <stanley.t.matthew@gmail.com>
2017-07-01 12:18:49 -0400

    adds basic user login functionality with personalized greeting

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib import auth
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -7,6 +8,12 @@ class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.credentials = {
+                    'username':'julia',
+                    'email': 'julia@example.com',
+                    'password': 'julia'
+                }
+        self.test_user = auth.models.User.objects.create_user(**self.credentials)
 
     def tearDown(self):
         self.browser.quit()
@@ -27,5 +34,31 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Welcome, Stranger!', header_text)
 
-        # he sees a link to login
+    def test_can_login(self):
+
+        # Julia wants a website where she can store her recipes, she has heard
+        # about stockpot and decides to try it out
+        self.browser.get(self.live_server_url)
+
+        # she sees a link to login which she clicks
         login_link = self.browser.find_element_by_id('login')
+        login_link.click()
+
+        # she is redirected to login page
+        self.assertIn('/accounts/login/', self.browser.current_url)
+
+        # she writes her username and password and clicks login
+        username_input = self.browser.find_element_by_id('id_username')
+        password_input = self.browser.find_element_by_id('id_password')
+        login_submit = self.browser.find_element_by_css_selector('input[type="submit"]')
+
+        username_input.send_keys('julia')
+        password_input.send_keys('julia') # silly julia that's a terrible password
+        login_submit.click()
+
+        # she sees that now the webpage welcomes her
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Welcome, julia!', header_text)
+
+
+
--- recipes/tests.py
+++ recipes/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.contrib import auth
 
 
 from recipes.views import home
@@ -20,7 +21,26 @@ class HomePageTest(TestCase):
 
 class LoginTests(TestCase):
 
+    def setUp(self):
+        self.credentials = {
+                    'username':'username',
+                    'email': 'email@example.com',
+                    'password': 'password'
+                }
+        self.test_user = auth.models.User.objects.create_user(**self.credentials)
+
 
     def test_login_urls_setup(self):
         response = self.client.get('/accounts/login/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    def test_user_can_login(self):
+        # should be AnonymousUser which is not authenticated
+        user = auth.get_user(self.client)
+        self.assertFalse(user.is_authenticated())
+
+        response = self.client.post('/accounts/login/', self.credentials, follow=True)
+
+        user = auth.get_user(self.client)
+        self.assertTrue(user.is_authenticated())

38cfbeee7bacd4d5bb1f76fb545ec6d3810488ae
Matt Stanley <stanley.t.matthew@gmail.com>
2017-10-18 17:20:12 -0400

    adds basic RecipeForm tests

--- recipes/test_forms.py
+++ recipes/test_forms.py
@@ -1,5 +1,22 @@
 from django.test import TestCase
 
+from .forms import RecipeForm
+
+
 class RecipeFormTest(TestCase):
 
-    pass
+
+    def test_valid_data(self):
+        form = RecipeForm({
+            'title': 'Tomato Soup',
+        })
+        self.assertTrue(form.is_valid())
+        recipe = form.save()
+        self.assertEqual(recipe.title, 'Tomato Soup')
+
+    def test_blank_data(self):
+        form = RecipeForm({})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'title': ['This field is required.'],
+        })
